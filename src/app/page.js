f2321965f3250a3857e4c0ecaf227c621aca8643
King <151782309+SarfDev@users.noreply.github.com>
2024-05-10 10:36:32 +0000

    okay

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,44 @@
-"use client"
-import { useEffect } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { socket } from "@/socketClient";
 
-
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [messaggi, setMessaggi] = useState([]);
   // Client side
   useEffect(() => {
-      socket.connect(() => {
-        console.log("Connected to server");
-        console.log(socket.id); // Stampa l'ID della connessione
-      });
+    socket.connect(() => {
+      console.log("Connected to server");
+      console.log(socket.id); // Stampa l'ID della connessione
+    });
 
-      return () => {
-        socket.disconnect();
-      };
+    return () => {
+      socket.disconnect();
+    };
   }, []);
+
+  socket.on("chat message", (msg) => {
+    setMessaggi((prev) => prev.push(msg));
+  });
 
   return (
     <>
       <main>
-        <button onClick={()=>{
-          socket.emit("chat message", "ciaoGino")
-        }}>Vai</button>
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
+
+        <button
+          onClick={() => {
+            socket.emit("chat message", input);
+          }}
+        >
+          Vai
+        </button>
+
+        <div>
+          {messaggi.map((e, i) => (
+            <p key={i}>{e}</p>
+          ))}
+        </div>
       </main>
     </>
   );
